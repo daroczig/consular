4abacacc369e4079b4fb14acfd705205274a6081
Gergely Daroczi (@daroczig) <daroczig@rapporter.net>
2018-08-13 11:12:27 +0200

    read and reload local agent's configuration

--- R/agent.R
+++ R/agent.R
@@ -1,8 +1,9 @@
-#' List consul agent members
+#' List members the local agent sees in the cluster gossip pool
 #' @return \code{data.table} with a member in each row
 #' @export
 #' @importFrom httr GET content
 #' @importFrom data.table rbindlist as.data.table
+#' @references \url{https://www.consul.io/api/agent.html#list-members}
 consular_agent_members <- function() {
     members <- content(GET(file.path(consular$agent_url, consular$version, 'agent', 'members')))
     tags <- rbindlist(lapply(members, function(member) {
@@ -16,4 +17,21 @@ consular_agent_members <- function() {
     cbind(members, tags)
 }
 
-## curl http://localhost:8500/v1/agent/services
+
+#' Show the configuration of the local agent
+#' @return \code{list}
+#' @export
+#' @importFrom httr GET content
+#' @references \url{https://www.consul.io/api/agent.html#read-configuration}
+consular_agent_configuration <- function() {
+    content(GET(file.path(consular$agent_url, consular$version, 'agent', 'self')))
+}
+
+
+#' Reloads the configuration of the local agent
+#' @export
+#' @importFrom httr PUT content
+#' @references \url{https://www.consul.io/api/agent.html#reaload-configuration}
+consular_agent_reload <- function() {
+    invisible(content(PUT(file.path(consular$agent_url, consular$version, 'agent', 'reload'))))
+}
